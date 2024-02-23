c61f582de0b24b627b2d2a3b422838572b3b2cc4
U_Aadarsh <106325783+2-0aadarsh@users.noreply.github.com>
2024-02-23 15:23:25 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,24 @@
+const accountId = "Aadarsh08";      /* declaring a constant */
+
+let accountEmail = "aadarsh0811@gmail.com";
+
+var accountName = "Aadarsh";
+
+accountCity = "Bhopal";
+
+// accountId = "2"  not allowed to assign value as it is a constant variable   
+// console.log(typeof(accountCity));
+// console.log(accountCity);
+
+// console.table([accountId,accountName,accountEmail,accountCity]);
+
+Geeks = 42;
+console.table([Geeks, typeof(Geeks)])
+
+Geeks = "A";
+console.table([Geeks, typeof(Geeks)])
+
+parseInt(Geeks);   // returns integer part of the number
+console.log("Integer Part : ", parseInt(Geeks)) ;
+
+console.log("Converting string into float");
